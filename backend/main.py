89ebb16ebb86ee423578b315fde7859abeba37d6
hasanemircanmetin <79004006+hasanemircanmetin@users.noreply.github.com>
2022-04-23 17:22:53 +0300

    ws cleaned

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,4 @@
+from time import sleep
 import serial
 from serial.tools import list_ports
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
@@ -6,32 +7,26 @@ from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 app = FastAPI()
 
 
-def get_port() -> serial.Serial:
-    ports = [p.device for p in list_ports.comports() if 'USB' in p.description]
-    if not ports:
-        raise IOError("Seri Baglantili cihaz yok!")
-    return serial.Serial(ports[0], 9600)
+# def get_port() -> serial.Serial:
+#     ports = [p.device for p in list_ports.comports() if 'USB' in p.description]
+#     if not ports:
+#         raise IOError("Seri Baglantili cihaz yok!")
+#     return serial.Serial(ports[0], 9600)
 
 
-ser = get_port()
+# ser = get_port()
 
 
 @app.websocket("/")
 async def websocket_endpoint(websocket: WebSocket):
-    websocket.accept()
+    await websocket.accept()
+    count = 0
 
     try:
         while True:
-            data = await websocket.receive_text()
-
-            if data == "97":
-                ser.write("a".encode())
-                print("a")
-            elif data == "98":
-                ser.write("b".encode())
-                print("b")
-            else:
-                print("[another commend]", data)
+            await websocket.send_text(str(count))
+            count += 1
+            sleep(1)
 
     except WebSocketDisconnect as e:
         print("disconnect", e)
@@ -40,6 +35,6 @@ if __name__ == '__main__':
     import uvicorn
     uvicorn.run(
         "main:app",
-        host="192.168.1.104",
         port=8001,
+        reload=True
     )
